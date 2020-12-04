492114d55a1682c55e74ec0a7c1d4db9fd13245e
dannykhoai <dvu2758@gmail.com>
2020-12-04 18:00:26 +0200

    Updates

---
+++ precache-manifest.c1f095f73761204cbcd55aff1bd1ff56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db444c38db10818b01d0db2930a2bab4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d468a393c18c4b0b3983",
+    "url": "/static/css/main.a66de6be.chunk.css"
+  },
+  {
+    "revision": "1b341ac55c2d4e5ca85e",
+    "url": "/static/js/2.77a6f29b.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.77a6f29b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d468a393c18c4b0b3983",
+    "url": "/static/js/main.95f0d9ae.chunk.js"
+  },
+  {
+    "revision": "5e11e5ba4e2efdfbf63d",
+    "url": "/static/js/runtime-main.001d78bf.js"
+  }
+]);
